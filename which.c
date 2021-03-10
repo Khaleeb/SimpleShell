806872dcd9cda4657e5e838a8376091854cf1936
Khaleeb <caldavis@udel.edu>
2021-03-10 18:18:20 -0500

    adding skeleton code

---
+++ which.c
@@ -0,0 +1,24 @@
+#include "get_path.h"
+
+char *which(char *command, struct pathelement *p)
+{
+  char cmd[64], *ch;
+  int  found;
+
+  found = 0;
+  while (p) {       
+    sprintf(cmd, "%s/%s", p->element, command);
+    if (access(cmd, X_OK) == 0) {
+      found = 1;
+      break;
+    }
+    p = p->next;
+  }
+  if (found) {
+    ch = malloc(strlen(cmd)+1);
+    strcpy(ch, cmd);
+    return ch;
+  }
+  else
+    return (char *) NULL;
+}
